8b84fae8852f7a1b46321cf30bbb027203ed6712
adrian tinaquero <adrian.tinaquero@udc.es>
2024-10-14 19:18:18 +0200

    Callback para la ventana importar CSV

--- src/DearPyGUI_test.py
+++ src/DearPyGUI_test.py
@@ -1,5 +1,17 @@
 import dearpygui.dearpygui as dpg
 
+
+# Callback para la ventana de importar archivo CSV
+def open_import_window(sender, app_data, user_data):
+    with dpg.window(label="Importar archivo CSV", modal=True, width=400, height=200) as import_window:
+        dpg.add_text("Introduzca aquí debajo la ruta del archivo CSV que desea importar:")
+        input_text = dpg.add_input_text(label="Ruta del CSV")
+        dpg.add_button(label="OK", callback=lambda: close_import_window(import_window))
+
+def close_import_window(window):
+    dpg.delete_item(window)
+
+
 # Ventana principal
 def main():
     with dpg.window(label="Ventana Principal", width=400, height=200):
