f037854e231ee5cdb7e01e67532d8145db0a194b
adrian tinaquero <adrian.tinaquero@udc.es>
2024-10-14 19:10:11 +0200

    crea ventana principal

--- src/PySimpleGUI_test.py
+++ src/PySimpleGUI_test.py
@@ -4,3 +4,20 @@ import PySimpleGUI as sg
 
 # Tema
 sg.theme('DarkTeal9')
+
+
+# Ventana principal
+def main():
+    layout = [
+        [sg.Text('Importar archivo CSV')],
+        [sg.Button('Importar archivo CSV'), sg.Button('Salir')]
+    ]
+    window =sg.Window('TEST', layout)
+    while True:
+        event, values = window.read()
+        if event == 'Salir' or event == sg.WINDOW_CLOSED:
+            break
+        if event == 'Importar archivo CSV':
+            import_window()
+            
+    window.close()
