09f2d52f39583f63868e9324c5501bf2abeb4b02
adrian tinaquero <adrian.tinaquero@udc.es>
2024-10-18 22:53:18 +0200

    importar librerías

---
+++ src/stasmodels_test.py
@@ -0,0 +1,6 @@
+# Importamos las librerías necesarias
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
