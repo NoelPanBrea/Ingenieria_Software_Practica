fe836a1b46aff8d7400eae9eb6992d72c457db46
adrian tinaquero <adrian.tinaquero@udc.es>
2024-10-18 22:54:02 +0200

    cargar archivo csv

--- src/stasmodels_test.py
+++ src/stasmodels_test.py
@@ -4,3 +4,10 @@ import pandas as pd
 import statsmodels.api as sm
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+
+# Cargar el dataset de ejemplo (diabetes)
+file_path = "C:\\Users\\Usuario\\Downloads\\housing.csv"
+housing_data = pd.read_csv(file_path)
+
+x = housing_data[['latitude']].values # Variables independientes
+y = housing_data['population'].values  # Variable dependiente
