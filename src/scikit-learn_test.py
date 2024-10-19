1b0318d138d6df36665d29020352e3fd168032b1
adrian tinaquero <adrian.tinaquero@udc.es>
2024-10-19 11:45:47 +0200

    importar datos csv

--- src/scikit-learn_test.py
+++ src/scikit-learn_test.py
@@ -6,3 +6,10 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
+
+# Cargar un dataset proporcionado por scikit-learn
+file_path = "C:\\Users\\Usuario\\Downloads\\housing.csv"
+housing_data = pd.read_csv(file_path)
+
+x = housing_data[['latitude']].values # Variables independientes
+y = housing_data['population'].values  # Variable dependiente
