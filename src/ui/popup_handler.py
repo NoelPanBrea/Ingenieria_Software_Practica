82c7e131a3d69a0bd482fed28379d260a65ab878
NoelPanBrea <npanbrea@gmail.com>
2024-11-21 16:46:46 +0100

    fix(popup_handler): format improved, deprecated code removed, useless imports removed

--- src/ui/popup_handler.py
+++ src/ui/popup_handler.py
@@ -1,30 +1,25 @@
 from PyQt5.QtWidgets import (
-QDialog, QDialogButtonBox, QLineEdit, QFormLayout, QMessageBox, QFileDialog)
-import PyQt5.QtCore as QtCore
+    QDialog, QDialogButtonBox, QLineEdit, QFormLayout, QMessageBox, QFileDialog)
+from PyQt5 import QtCore
+
 
 class InputDialog(QDialog):
-    def __init__(self, labels: list[str], title: str = 'default title' , stylesheet: str = None, parent = None):
-        super().__init__(parent,  QtCore.Qt.WindowCloseButtonHint)
-        buttonBox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel, self)
-        layout = QFormLayout(self)
-        self.setWindowTitle(title)
-        from PyQt5.QtWidgets import QDialog, QDialogButtonBox, QLineEdit, QFormLayout
-from PyQt5.QtCore import Qt, pyqtSlot
+    def __init__(self, labels: list[str], title: str = 'default title',
+        stylesheet: str = None, parent=None):
 
-class InputDialog(QDialog):
-    def __init__(self, labels: list[str], title: str = 'default title', stylesheet: str = None, parent=None):
         super().__init__(parent, QtCore.Qt.WindowCloseButtonHint)
-        
+
         # Configuración del botón de Aceptar y Cancelar
-        buttonBox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel, self)
-        
+        buttonBox = QDialogButtonBox(
+            QDialogButtonBox.Ok | QDialogButtonBox.Cancel, self)
+
         layout = QFormLayout(self)
         self.setWindowTitle(title)
 
         # Si hay una hoja de estilo personalizada, aplicarla
         if stylesheet:
             self.setStyleSheet(stylesheet)
-        
+
         self.inputs = []
         for lab in labels:
             line_edit = QLineEdit(self)
@@ -40,7 +35,8 @@ class InputDialog(QDialog):
     def get_inputs(self):
         return tuple(input.text() for input in self.inputs)
 
-def show_message(message: str, parent = None) -> None:
+
+def show_message(message: str, parent=None) -> None:
     """
     Shows a message in a pop up window
 
@@ -53,13 +49,14 @@ def show_message(message: str, parent = None) -> None:
     -----------
         None
     """
-    msg_box = QMessageBox(parent = parent)
+    msg_box = QMessageBox(parent=parent)
     msg_box.setIcon(QMessageBox.Information)
     msg_box.setText(message)
     msg_box.setWindowTitle("Éxito")
     msg_box.exec_()
 
-def show_error(message: str, parent = None) -> None:
+
+def show_error(message: str, parent=None) -> None:
     """
     Shows an error message in a pop up window
 
@@ -72,13 +69,14 @@ def show_error(message: str, parent = None) -> None:
     -----------
         None
     """
-    error_msg = QMessageBox(parent = parent)
+    error_msg = QMessageBox(parent=parent)
     error_msg.setIcon(QMessageBox.Critical)
     error_msg.setText(message)
     error_msg.setWindowTitle('Error')
     error_msg.exec_()
 
-def open_file_dialog(parent = None) -> None:
+
+def open_file_dialog(parent=None) -> None:
     """
     Shows the dialog to open files and returns the selected directory
 
@@ -90,10 +88,11 @@ def open_file_dialog(parent = None) -> None:
     res = 'Todos los Archivos (*.*);;Archivos CSV (*.csv);;Archivos Excel'
     res += ' (*.xlsx *.xls);;Base de datos SQLite (*.sqlite *.db)'
     file_path, _ = QFileDialog.getOpenFileName(parent, 'Seleccionar archivo',
-        '', res, options = options)
+                                               '', res, options=options)
     return file_path
 
-def save_file_dialog(parent = None) -> None:
+
+def save_file_dialog(parent=None) -> None:
     """
     Shows the dialog to save files and returns the selected directory
 
@@ -104,6 +103,5 @@ def save_file_dialog(parent = None) -> None:
     options = QFileDialog.Options()
     res = 'Archivo joblib (*.joblib)'
     file_path, _ = QFileDialog.getSaveFileName(parent, 'Guardar archivo',
-        '', res, options = options)
+                                               '', res, options=options)
     return file_path
-
