58bb3bcc35520d3270ec4004debd66de0933b8c2
amcarnota <ainhoacarnota@gmail.com>
2024-11-24 22:23:02 +0100

    fix(input_dialog_test): minor error in module name

--- src/tests/input_dialog_test.py
+++ src/tests/input_dialog_test.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.insert(0, 'src')
-from tabs.data_aux.common_aux.popup_handler import InputDialog
+from ui.popup_handler import InputDialog
 from PyQt5.QtWidgets import QApplication
 from PyQt5 import QtCore
 if __name__ == '__main__':
